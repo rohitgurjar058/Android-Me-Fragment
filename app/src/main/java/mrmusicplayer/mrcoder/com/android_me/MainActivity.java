e71c0e5718b199bd52191d02c2bf7b9c99a09c9f
rohitgurjar058 <rohitgurjar.jec@gmail.com>
2018-05-19 14:44:47 +0530

    Initial commit

---
+++ app/src/main/java/mrmusicplayer/mrcoder/com/android_me/MainActivity.java
@@ -0,0 +1,13 @@
+package mrmusicplayer.mrcoder.com.android_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
