a614e905724ebacf31da7e32755efe3b3f36a50c
rohitgurjar058 <rohitgurjar.jec@gmail.com>
2018-05-19 22:28:36 +0530

    Complete Implementation of BodyPartFragment with listeter

--- app/src/main/java/mrmusicplayer/mrcoder/com/android_me/MainActivity.java
+++ app/src/main/java/mrmusicplayer/mrcoder/com/android_me/MainActivity.java
@@ -1,7 +1,11 @@
 package mrmusicplayer.mrcoder.com.android_me;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mrmusicplayer.mrcoder.com.android_me.data.AndroidImageAssets;
+import mrmusicplayer.mrcoder.com.android_me.ui.BodyPartFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            //Create an Instance of BodypartFragment
+            BodyPartFragment headFragment = new BodyPartFragment();
+
+            // Set the list of image id's for the head fragment and set the position to the second image in the list
+            headFragment.setImageIds(AndroidImageAssets.getHeads());
+
+            //Add the fragment to its container using FragmnentManager and a Transaction
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.head_container, headFragment)
+                    .commit();
+
+            //Display body and leg fragment
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction()
+                    .add(R.id.body_container, bodyFragment)
+                    .commit();
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction()
+                    .add(R.id.leg_container, legFragment)
+                    .commit();
+        }
     }
 }
